1ded72f93622250f1e3cb4c85107577ac9349ae5
emilmarcuarez <emilmarpatricia@gmail.com>
2023-12-26 14:00:06 -0400

    ambiente de desarrollo listo

---
+++ router.php
@@ -0,0 +1,64 @@
+<?php
+namespace MVC;
+class Router{
+
+    public $rutasGET=[];
+    public $rutasPOST=[];
+
+    public function get($url,$fn){
+        $this->rutasGET[$url]=$fn;
+    }
+    public function post($url,$fn){
+        $this->rutasPOST[$url]=$fn;
+    }
+    public function comprobarRutas(){
+
+        session_start();
+        $auth=$_SESSION['login'] ?? null;
+
+        // ARREGLO DE RUTAS PROTEGIDAS
+        $rutas_protegidas=['/admin', '/locales/crear', '/locales/actualizar', '/locales/eliminar'];
+
+        // basada en la url que estoy visitando gracias al router, me busca la funcion asociada a ese url
+        $urlActual=$_SERVER['PATH_INFO'] ?? '/';
+        $metodo =$_SERVER['REQUEST_METHOD'];
+        //    para validar la ruta
+        if($metodo==='GET'){
+           $fn=$this->rutasGET[$urlActual] ?? null;
+           
+        }else {
+            $fn=$this->rutasPOST[$urlActual] ?? null;
+        }
+
+        // PROTEGER LAS RUTAS
+        if(in_array($urlActual, $rutas_protegidas)&& !$auth){ ///si no esta autenticado
+            header('location: /');
+        }
+
+        if($fn){
+            // la url existe y hay una funcion asociada
+            // cuando no sabemos el nombre de la funcion, esta la busca
+            // debuguear($fn);
+            call_user_func($fn,$this);
+
+        }else{
+            // debuguear($fn);
+            echo 'Error 404: Pagina No encontrada';
+        }
+    }
+
+
+    // muestra una vista
+    public function render($view, $datos=[]){
+
+       foreach($datos as $key => $value){
+        $$key =$value;
+        
+       }
+     
+        ob_start();//empieza almacenar datos en memoria
+        include __DIR__ . "/views/$view.php";
+        
+        $contenido=ob_get_clean();//limpia el buffer
+
+        include __DIR__ . "/views/layout.php"
