e4e21f64a28be0d0fc88bc5a63d88e93e0813eab
emilmarcuarez <emilmarpatricia@gmail.com>
2024-04-02 00:12:31 -0400

    cambio

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php 
-error_reporting(E_ALL);
-ini_set('ignore_repeated_errors', TRUE);
-ini_set('display_errors', FALSE);
-ini_set('log_errors', TRUE);
-ini_set("error_log", 'debug.log');
+// error_reporting(E_ALL);
+// ini_set('ignore_repeated_errors', TRUE);
+// ini_set('display_errors', FALSE);
+// ini_set('log_errors', TRUE);
+// ini_set("error_log", 'debug.log');
 
 
 require_once __DIR__ . '/../includes/app.php';
